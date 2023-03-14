8862579be425f68555f0e4cbfbfab5512da85d23
kenorb <kenorb@users.noreply.github.com>
2023-03-14 23:25:14 +0000

    Adds Solana integration to land asset

--- NFT/Lands/Land.cs
+++ NFT/Lands/Land.cs
@@ -1,13 +1,21 @@
-using System.Collections;
+using System.Threading.Tasks;
+using Solana.Unity.Rpc;
+using Solana.Unity.Rpc.Models;
+using Solana.Unity.Wallet;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class Land : MonoBehaviour {
 
   // Static variable to keep track of attached instances of this script
-  private static bool scriptAttached = false;
-  public int xPos = 0;
-  public int yPos = 0;
+  private Account accountInfo;
+  private List<Solana.Unity.Wallet.Account> accountInfoList;
+  private bool scriptAttached = false;
+  private static readonly IRpcClient rpcSolanaClient = ClientFactory.GetClient(Cluster.MainNet);
+  public int xPos = 0; // Position X.
+  public int yPos = 0; // Position Y.
+  public string nftSolanaAddress = ""; // NFT Solana address.
 
   void Awake() {
     // Check if this script is already attached to the GameObject.
@@ -21,14 +29,65 @@ public class Land : MonoBehaviour {
     scriptAttached = true;
   }
 
+  public async void GetAccountInfo()
+  {
+      // Convert the NFT address string to a PublicKey object.
+      PublicKey nftPublicKey = new PublicKey(nftSolanaAddress);
+      // Get the account info of the NFT.
+      var accountInfoResult = await rpcSolanaClient.GetAccountInfoAsync(nftPublicKey);
+      if (accountInfoResult.WasSuccessful) {
+        //accountInfo = accountInfoResult.Result.Value.Data;
+        //accountInfoList = new List<Solana.Unity.Wallet.Account>();
+        List<AccountInfo> accountInfoList = new List<AccountInfo>();
+        accountInfoList.Add(accountInfoResult.Result.Value);
+      }
+      else
+      {
+          Debug.LogError($"Failed to get account info: {accountInfoResult}");
+      }
+      PrintAccountInfo(accountInfoList);
+      // accountInfo = await rpcSolanaClient.GetAccountInfoAsync(nftPublicKey);
+  }
+
   void OnDestroy() {
     // Reset the scriptAttached variable when the script is destroyed.
     scriptAttached = false;
   }
 
+  public void PrintAccountInfo(List<Solana.Unity.Wallet.Account> accountInfoList)
+  {
+      foreach(var accountInfo in accountInfoList)
+      {
+          Debug.Log($"Account owner: {accountInfo.PublicKey}");
+      }
+  }
+
   // Start is called before the first frame update.
-  void Start() {}
+  void Start() {
+    InvokeRepeating("UpdatePerMinute", 2.0f, 60.0f);
+    InvokeRepeating("UpdatePerSecond", 1.0f, 1.0f);
+  
+    if (nftSolanaAddress.Length > 0) {
+      Debug.Log("NFT: " + nftSolanaAddress);
+      GetAccountInfo();
+    }
+  }
 
   // Update is called once per frame.
   void Update() {}
+
+  // Update is called once per minute.
+  void UpdatePerMinute() {
+    if (nftSolanaAddress.Length > 0) {
+      Debug.Log("NFT3: " + nftSolanaAddress);
+    }
+  }
+
+  // Update is called once per second.
+  void UpdatePerSecond() {
+    if (nftSolanaAddress.Length > 0) {
+      // Debug.Log("NFT2: " + nftSolanaAddress);
+    }
+  }
+
 }
